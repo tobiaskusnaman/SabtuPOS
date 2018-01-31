195c09237871831f45d6861d3f25bc2272e54631
Tobias Kusnaman <tobiaskusnaman@Tobiass-MacBook-Pro.local>
2018-01-31 08:58:06 +0700

    login done

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const RouteProduct = require('./routers/product');
 const RouteHome = require('./routers/home');
 const RouteOrder = require('./routers/order');
 const RouteUser = require('./routers/user');
+const session = require('express-session')
+
+app.use(session({
+  secret: 'keyboard cat'
+}))
 
 app.use('/product',RouteProduct);
 app.use('/user',RouteUser);
 app.use('/', RouteHome)
 app.use('/order', RouteOrder)
 app.use('/product', RouteProduct);
+
+app.get('/signOut', (req,res)=>{
+  req.session.destroy(err=>{
+    if (!err) {
+      res.redirect('/')
+    } else {
+      res.send(err)
+    }
+  })
+  res.send('logout men')
+})
 
 
 app.listen(3000);
